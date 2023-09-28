c3c76bd4c163fa9fc24d6d11959a96665a843fc9
ColoNOB <aconocchiari@gmail.com>
2023-09-28 18:11:01 -0300

    Subiendo

---
+++ Script/details.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const detallePrincipal = document.getElementById('detalle-principal');
+    const volverButton = document.getElementById('volver');
+  
+    const productoId = localStorage.getItem('productoSeleccionado');
+  
+    if (productoId) {
+      fetch('/Script/products.json')
+        .then(response => response.json())
+        .then(data => {
+          const producto = data.find(p => p.id === parseInt(productoId));
+          if (producto) {
+            detallePrincipal.innerHTML = `
+              <h1>${producto.titulo}</h1>
+              <img src="${producto.detalle.imagenes}" alt="${producto.titulo}">
+              <p class="descripcion">${producto.detalle.descripcionExtendida}</p>
+              <p class="precio">${producto.detalle.precio}</p>
+              <button class="comprar btn btn-dark">Comprar</button>
+            `;
+          }
+        });
+    }
+  
+    volverButton.addEventListener('click', () => {
+      window.location.href = 'index.html';
+    });
+  });
+  
---
+++ Script/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const productosContainer = document.getElementById('productos-container');
+  
+    fetch('/Script/products.json')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(producto => {
+          const productoCard = document.createElement('div');
+          productoCard.classList.add('cards');
+          productoCard.innerHTML = `
+            <h2>${producto.titulo}</h2>
+            <img src="${producto.detalle.imagenes}" alt="${producto.titulo}">
+            <h3>${producto.detalle.precio}</h3>
+            <button class="ver-detalle btn btn-dark" data-id="${producto.id}">Ver Detalle</button>
+          `;
+          productosContainer.appendChild(productoCard);
+        });
+      });
+  
+    productosContainer.addEventListener('click', (event) => {
+      if (event.target.classList.contains('ver-detalle')) {
+        const productoId = event.target.getAttribute('data-id');
+        localStorage.setItem('productoSeleccionado', productoId);
+        window.location.href = `details.html?id=${productoId}`;
+      }
+    });
+  });
+  
